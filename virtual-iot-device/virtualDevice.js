21d4e83dc8d86639276503e8c758f762cb018455
Oguz Tecirlioglu <tecirliogluoguz@gmail.com>
2023-07-19 16:06:10 +0100

    Updated environment variables, added main method

--- virtual-iot-device/virtualDevice.js
+++ virtual-iot-device/virtualDevice.js
@@ -3,16 +3,16 @@ const express = require("express");
 const utils = require("./utils");
 
 /** @global */
-const apiEndpoint = utils.envOrDefault("VIRTUAL_DEVICE_API_ENDPOINT", "/api/post");
+const API_ENDPOINT = utils.envOrDefault("VIRTUAL_DEVICE_API_ENDPOINT", "/api/post");
 
 /** @global */
-const intervalSeconds = utils.envOrDefault("VIRTUAL_DEVICE_INTERVAL_SECONDS", 10);
+const INTERVAL_SECONDS = utils.envOrDefault("VIRTUAL_DEVICE_INTERVAL_SECONDS", 10);
 
 /** @global */
-const port = utils.envOrDefault("VIRTUAL_DEVICE_PORT", 3000);
+const PORT = utils.envOrDefault("VIRTUAL_DEVICE_PORT", 3000);
 
 /** @global */
-const randomnessAmount = utils.envOrDefault("VIRTUAL_DEVICE_RANDOMNESS_AMOUNT", 0);
+const RANDOMNESS_AMOUNT = utils.envOrDefault("VIRTUAL_DEVICE_RANDOMNESS_AMOUNT", 0);
 
 let lastJSONArray = [];
 
@@ -23,11 +23,11 @@ app.use(express.json());
  * Async method that sends randomly generated data to the API endpoint which was configured by reading environment variables.
  */
 async function sendData() {
-  const data = utils.generateRandomData(randomnessAmount);
+  const data = utils.generateRandomData(RANDOMNESS_AMOUNT);
 
   try {
-    await axios.post(apiEndpoint, data, {
-      baseURL: `http://localhost:${port}`,
+    await axios.post(API_ENDPOINT, data, {
+      baseURL: `http://localhost:${PORT}`,
     });
     console.log("Data sent:", data);
   } catch (error) {
@@ -35,19 +35,25 @@ async function sendData() {
   }
 }
 
-setInterval(sendData, intervalSeconds * 250);
+// NodeJS equivalent of creating a "main" method.
+if (require.main === module) {
+  setInterval(sendData, INTERVAL_SECONDS * 250);
 
-// Created for testing purposes, the virtual device makes a http post request to itself
-// which gets appended to an array.
-app.post(apiEndpoint, (req, res) => {
-  lastJSONArray.push(req.body);
-  res.status(200).send("Data recieved succesfully");
-});
+  // Created for testing purposes, the virtual device makes a http post request to itself
+  // which gets appended to an array.
+  app.post(API_ENDPOINT, (req, res) => {
+    lastJSONArray.push(req.body);
+    res.status(200).send("Data recieved succesfully");
+  });
 
-app.get("/api/allData", (req, res) => {
-  res.send(lastJSONArray);
-});
+  app.get("/api/allData", (req, res) => {
+    res.send(lastJSONArray);
+  });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log(
+      `Configuration variables are: API_ENDPOINT=${API_ENDPOINT}, INTERVAL_SECONDS=${INTERVAL_SECONDS}, PORT=${PORT}, RANDOMNESS_AMOUNT=${RANDOMNESS_AMOUNT}`
+    );
+  });
+}
