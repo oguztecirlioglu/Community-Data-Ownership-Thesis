25f0b5f088628c3b2a3ed3c2645429a4f724f468
Oguz Tecirlioglu <tecirliogluoguz@gmail.com>
2023-07-19 15:57:45 +0100

    Added jsdoc to automatically create documentation

--- virtual-iot-device/virtualDevice.js
+++ virtual-iot-device/virtualDevice.js
@@ -2,25 +2,32 @@ const axios = require("axios");
 const express = require("express");
 const utils = require("./utils");
 
-const apiEndpoint = utils.envOrDefault(
-  "VIRTUAL_DEVICE_API_ENDPOINT",
-  "/api/post"
-); // Replace with your API endpoint URL
-const intervalSeconds = utils.envOrDefault(
-  "VIRTUAL_DEVICE_INTERVAL_SECONDS",
-  10
-); // Adjust the interval as per your requirements
+/** @global */
+const apiEndpoint = utils.envOrDefault("VIRTUAL_DEVICE_API_ENDPOINT", "/api/post");
+
+/** @global */
+const intervalSeconds = utils.envOrDefault("VIRTUAL_DEVICE_INTERVAL_SECONDS", 10);
+
+/** @global */
 const port = utils.envOrDefault("VIRTUAL_DEVICE_PORT", 3000);
-const app = express();
-app.use(express.json());
+
+/** @global */
+const randomnessAmount = utils.envOrDefault("VIRTUAL_DEVICE_RANDOMNESS_AMOUNT", 0);
+
 let lastJSONArray = [];
 
+const app = express();
+app.use(express.json());
+
+/**
+ * Async method that sends randomly generated data to the API endpoint which was configured by reading environment variables.
+ */
 async function sendData() {
-  const data = utils.generateRandomData();
+  const data = utils.generateRandomData(randomnessAmount);
 
   try {
     await axios.post(apiEndpoint, data, {
-      baseURL: `http://localhost:${port}`, // Dynamically determine the base URL using req.protocol, req.hostname, and port
+      baseURL: `http://localhost:${port}`,
     });
     console.log("Data sent:", data);
   } catch (error) {
@@ -31,7 +38,7 @@ async function sendData() {
 setInterval(sendData, intervalSeconds * 250);
 
 // Created for testing purposes, the virtual device makes a http post request to itself
-// which gets appended to an array. 
+// which gets appended to an array.
 app.post(apiEndpoint, (req, res) => {
   lastJSONArray.push(req.body);
   res.status(200).send("Data recieved succesfully");
