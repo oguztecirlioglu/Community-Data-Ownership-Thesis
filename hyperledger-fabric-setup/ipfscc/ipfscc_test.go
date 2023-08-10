812a3f97fa9c678689bffa4a11850fb1dde69b36
Oguz Tecirlioglu <tecirliogluoguz@gmail.com>
2023-08-10 01:15:53 +0100

    Added simple tests, updated gitignore

---
+++ hyperledger-fabric-setup/ipfscc/ipfscc_test.go
@@ -0,0 +1,65 @@
+package Main_test
+
+import (
+	"encoding/base64"
+	Main "ipfscc"
+	"ipfscc/mocks"
+	"os"
+	"testing"
+
+	"github.com/hyperledger/fabric-chaincode-go/pkg/cid"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/stretchr/testify/require"
+)
+
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o mocks/transaction.go -fake-name TransactionContext . transactionContext
+type transactionContext interface {
+	contractapi.TransactionContextInterface
+}
+
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o mocks/chaincodestub.go -fake-name ChaincodeStub . chaincodeStub
+type chaincodeStub interface {
+	shim.ChaincodeStubInterface
+}
+
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o mocks/statequeryiterator.go -fake-name StateQueryIterator . stateQueryIterator
+type stateQueryIterator interface {
+	shim.StateQueryIteratorInterface
+}
+
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o mocks/clientIdentity.go -fake-name ClientIdentity . clientIdentity
+type clientIdentity interface {
+	cid.ClientIdentity
+}
+
+const myOrg1Msp = "Org1Testmsp"
+const myOrg1Clientid = "myOrg1Userid"
+
+const testDeviceName = "testDevice_4000"
+const testCID = "42421337"
+const testDataDate = "02-02-2000"
+
+func TestUploadAssetOkay(t *testing.T) {
+	transactionContext, _ := prepMocks(myOrg1Msp, myOrg1Clientid)
+	assetTransferCC := Main.SmartContract{}
+
+	// No transient map
+	err := assetTransferCC.UploadDataAsAsset(transactionContext, testDeviceName, testCID, testDataDate)
+	require.NoError(t, err)
+
+}
+
+func prepMocks(orgMSP, clientId string) (*mocks.TransactionContext, *mocks.ChaincodeStub) {
+	chaincodeStub := &mocks.ChaincodeStub{}
+	transactionContext := &mocks.TransactionContext{}
+	transactionContext.GetStubReturns(chaincodeStub)
+
+	clientIdentity := &mocks.ClientIdentity{}
+	clientIdentity.GetMSPIDReturns(orgMSP, nil)
+	clientIdentity.GetIDReturns(base64.StdEncoding.EncodeToString([]byte(clientId)), nil)
+	// set matching msp ID using peer shim env variable
+	os.Setenv("CORE_PEER_LOCALMSPID", orgMSP)
+	transactionContext.GetClientIdentityReturns(clientIdentity)
+	return transactionContext, chaincodeStub
+}
